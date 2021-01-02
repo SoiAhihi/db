4ec808a4267aba01f0cbbf5db558bc42afa5fe19
Cao Bảo <caongocbao123@gmail.com>
2021-01-02 15:04:27 +0700

    first

---
+++ ass2/DAO/EmployeeDAO.cs
@@ -0,0 +1,35 @@
+﻿using ass2.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass2.DAO
+{
+    class EmployeeDAO
+    {
+        private static EmployeeDAO instance; // Ctrl + R + E
+
+        public static EmployeeDAO Instance
+        {
+            get { if (instance == null) instance = new EmployeeDAO(); return EmployeeDAO.instance; }
+            private set { EmployeeDAO.instance = value; }
+        }
+
+        private EmployeeDAO() { }
+        public Employee SelectNVByID(int id)
+        {
+            Employee re;
+            string query = "SELECT * FROM EMPLOYEE WHERE id = @id";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query, new object[]{ id});
+            foreach (DataRow item in data.Rows)
+            {
+                re = new Employee(item);
+                return re;
+            }
+            return null;
+        }
+    }
+}
